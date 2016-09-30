511c39518e9426610f586c29641808609b042047
Anton Kuksov <akuksov@klass2-14-l.intra.itcollege.ee>
2016-09-30 09:53:20 +0300

    PR6, PR7

---
+++ praktikum6/Meetodid.java
@@ -0,0 +1,41 @@
+package praktikum6;
+
+
+import lib.TextIO;
+
+public class Meetodid {
+	
+	
+	public static int kasutajaSisestus(int min, int max) {
+		System.out.println("Siseta arv" + min + " kuni " + max);
+		int sissetus = TextIO.getlnInt();	
+		return sissetus;
+	}
+
+	public static void main(String[] args) {
+		
+		int kasutajaSisestusArv = kasutajaSisestus(0, 10);
+		System.out.println("Kasutaja sisestas" + kasutajaSisestusArv);
+		
+		/*System.out.println("Tere! Sisseta arv");
+		int arv = TextIO.getlnInt();	
+		int vastus = ruut(arv);
+		System.out.println("Arv ruudus on" + vastus);
+		int vastus2 = ruut(arv);
+		System.out.println("Arv ruudus on" + vastus2);
+		*/
+	}
+	public static int ruut(int i) {
+		return i*i*i;
+		
+	}
+	public static int kuup(int i) {
+		return (int) Math.pow(i, 3);
+		
+	}
+	
+
+		
+	
+
+}
---
+++ praktikum7/GuessNum.java
@@ -0,0 +1,42 @@
+package praktikum7;
+
+import java.util.Random;
+
+import lib.TextIO;
+
+public class GuessNum {
+
+	public static void main(String[] args) {
+		Random rand = new Random();
+		int num1 = rand.nextInt(5) + 1;
+
+		while (true) {
+
+			System.out.println("Try");
+			int guess = TextIO.getlnInt();
+			Kontroll(num1, guess);
+			if (num1 == guess) {
+				break;
+			}
+
+		}
+
+	}
+
+	public static void Kontroll(int num1, int guess) {
+
+		if (num1 == guess) {
+			System.out.println("You Win!");
+		}
+
+		else if (num1 > guess) {
+			System.out.println("It's small. Close enough");
+
+		} else {
+			System.out.println("It's big. Try again");
+		}
+
+		return;
+	}
+
+}
