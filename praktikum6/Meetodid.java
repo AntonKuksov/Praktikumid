948dbf12f2f7e0f2882c66b592ae478bc54bbca7
Anton <Anton@1PC>
2016-10-05 09:42:16 +0300

    Ruut ja kuup

--- praktikum6/Meetodid.java
+++ praktikum6/Meetodid.java
@@ -6,27 +6,27 @@ import lib.TextIO;
 public class Meetodid {
 	
 	
-	public static int kasutajaSisestus(int min, int max) {
-		System.out.println("Siseta arv" + min + " kuni " + max);
+	/*public static int kasutajaSisestus(int min, int max) {
+		System.out.println("Siseta arv " + min + " kuni " + max);
 		int sissetus = TextIO.getlnInt();	
 		return sissetus;
 	}
+	*/
 
 	public static void main(String[] args) {
 		
-		int kasutajaSisestusArv = kasutajaSisestus(0, 10);
-		System.out.println("Kasutaja sisestas" + kasutajaSisestusArv);
-		
-		/*System.out.println("Tere! Sisseta arv");
+	/*	int kasutajaSisestusArv = kasutajaSisestus(0, 10);
+		System.out.println("Kasutaja sisestas " + kasutajaSisestusArv);
+	*/
+		System.out.println("Tere! Sisseta arv");
 		int arv = TextIO.getlnInt();	
 		int vastus = ruut(arv);
-		System.out.println("Arv ruudus on" + vastus);
-		int vastus2 = ruut(arv);
-		System.out.println("Arv ruudus on" + vastus2);
-		*/
+		System.out.println("Arv ruudus on " + vastus);
+		int vastus2 = kuup(arv);
+		System.out.println("Arv kuup on " + vastus2);
 	}
 	public static int ruut(int i) {
-		return i*i*i;
+		return i*i;
 		
 	}
 	public static int kuup(int i) {
