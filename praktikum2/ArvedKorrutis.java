1b3f7095d28a0ddee00f8b155b4f45710a6a5d2a
Anton <Anton@1PC>
2016-10-05 09:52:51 +0300

    test

--- praktikum2/ArvedKorrutis.java
+++ praktikum2/ArvedKorrutis.java
@@ -8,7 +8,7 @@ public class ArvedKorrutis {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Palun sisesta kaks arvu.");
+		System.out.println("Palun sisesta kaks arvu!!!!");
 		System.out.println("Arvude korrutis on: " + TextIO.getlnInt() * TextIO.getlnInt());
 		
 /*		int arv1;
